f9439d0a60586c454641c3bf81e80ea02752bf44
Scott Forbes <sf1179@gmail.com>
2014-02-24 10:38:41 -0600

    Improved admin functionality for loading new adventures and new stories

--- adventure/admin.py
+++ adventure/admin.py
@@ -3,25 +3,46 @@ from django.conf.urls.defaults import *
 from django.http import HttpResponse
 from django.utils.translation import ugettext_lazy as _
 from sssd.adventure.models import *
+from sssd.adventure.forms import StoryDetailFormset, LevelCharacterFormset
 
 class WordInline(admin.TabularInline):
-    model = Word
-    extra = 3
+	model = Word
+	extra = 3
 
 class WordGroupAdmin(admin.ModelAdmin):
-    inlines = [WordInline]      
+	inlines = [WordInline]	  
 
 class StoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'level',
-        'character',
-        'wordgroup',
-        'next_level',
-    )
+	list_display = (
+		'level',
+		'character',
+		'wordgroup',
+		'next_level',
+	)
+
+class StoryInline(admin.TabularInline):
+	model = Story
+	formset = StoryDetailFormset
+	extra = 0
+	fk_name = "level"
+
+class LevelCharacterInline(admin.TabularInline):
+	model = LevelCharacter
+	formset = LevelCharacterFormset
+	extra = 0
+
+class LevelAdmin(admin.ModelAdmin):
+	inlines = [StoryInline, LevelCharacterInline]
+	list_display = (
+		'game',
+		'level_descr',
+	)
+
+	list_filter = ('game',)
 
 admin.site.register(Game)
 admin.site.register(Character)
-admin.site.register(Level)
+admin.site.register(Level, LevelAdmin)
 admin.site.register(LevelCharacter)
 admin.site.register(WordGroup, WordGroupAdmin)
 admin.site.register(Word)
---
+++ adventure/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+
+from sssd.adventure.models import *
+
+class StoryDetailFormset(forms.models.BaseInlineFormSet):
+	def __init__(self, *args, **kwargs):
+		super(StoryDetailFormset, self).__init__(*args, **kwargs)
+		for form in self.forms:
+			self.update_choices(form)
+	
+	# We need to override the constructor (and the associated property) for the
+	# empty form, so dynamic forms work.
+	def _get_empty_form(self, **kwargs):
+		form = super(StoryDetailFormset, self)._get_empty_form(**kwargs)
+		self.update_choices(form)
+		return form
+	empty_form = property(_get_empty_form)
+	
+	# This updates one form's 'character' and 'level' field queryset, if there is a game
+	# associated with the formset. Otherwise, make the choice list contain all levels and characters
+	def update_choices(self, form):
+		if 'game' in self.data:
+			characters = Game.objects.get(pk=self.data['game']).character_set.all()
+			levels = Game.objects.get(pk=self.data['game']).level_set.all()
+		elif self.instance.pk and self.instance.game:
+			characters = self.instance.game.character_set.all()
+			levels = self.instance.game.level_set.all()
+		else:
+			characters = Character.objects.all()
+			levels = Level.objects.all()
+		
+		form.fields['character'].queryset = characters
+		form.fields['next_level'].queryset = levels
+
+class LevelCharacterFormset(forms.models.BaseInlineFormSet):
+	def __init__(self, *args, **kwargs):
+		super(LevelCharacterFormset, self).__init__(*args, **kwargs)
+		for form in self.forms:
+			self.update_choices(form)
+	
+	def _get_empty_form(self, **kwargs):
+		form = super(LevelCharacterFormset, self)._get_empty_form(**kwargs)
+		self.update_choices(form)
+		return form
+	empty_form = property(_get_empty_form)
+	
+	def update_choices(self, form):
+		if 'game' in self.data:
+			characters = Game.objects.get(pk=self.data['game']).character_set.all()
+		elif self.instance.pk and self.instance.game:
+			characters = self.instance.game.character_set.all()
+		else:
+			characters = Character.objects.all()
+			levels = Level.objects.all()
+		
+		form.fields['character'].queryset = characters	
